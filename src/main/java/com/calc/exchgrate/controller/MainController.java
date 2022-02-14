3a80eec48b5886593f1b704756b04195112e9aed
LG <LG@10.8.13.201>
2022-02-14 09:43:02 +0900

    MainController 미사용 소스 정리

--- src/main/java/com/calc/exchgrate/controller/MainController.java
+++ src/main/java/com/calc/exchgrate/controller/MainController.java
@@ -1,25 +1,15 @@
 package com.calc.exchgrate.controller;
 
-import java.util.Locale;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.calc.exchgrate.service.ExchgrateCalcService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class MainController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
-	@Autowired
-	ExchgrateCalcService exchgrateCalcService;
 	
 	//root 경로 접속 controller
 	@RequestMapping(value = "/", method = RequestMethod.GET)
